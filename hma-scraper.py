66b3509ec8d51d971317b3cc68f8ee9f0e9809ca
Daytona <IDSninja@users.noreply.github.com>
2016-02-01 18:41:59 -0700

    Create hma-scraper.py

---
+++ hma-scraper.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import requests
+import re
+
+def get_list(uri):
+
+    r = requests.get('http://proxylist.hidemyass.com/'+uri)
+
+    bad_class="("
+    for line in r.text.splitlines():
+        class_name = re.search(r'\.([a-zA-Z0-9_\-]{4})\{display:none\}', line)
+        if class_name is not None:
+           bad_class += class_name.group(1)+'|'
+
+
+    bad_class = bad_class.rstrip('|')
+    bad_class += ')'
+
+    to_remove = '(<span class\="'+ bad_class + '">[0-9]{1,3}</span>|<span style=\"display:(none|inline)\">[0-9]{1,3}</span>|<div style="display:none">[0-9]{1,3}</div>|<span class="[a-zA-Z0-9_\-]{1,4}">|</?span>|<span style="display: inline">)'
+
+    junk = re.compile(to_remove, flags=re.M)
+    junk = junk.sub('', r.text)
+    junk = junk.replace("\n", "")
+
+    proxy_src = re.findall('([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})\s*</td>\s*<td>\s*([0-9]{2,6}).{100,1200}(socks4/5|HTTPS?)', junk)
+
+    for src in proxy_src:
+        if src[2] == 'socks4/5':
+            proto = 'socks5h'
+        else:
+            proto = src[2].lower()
+        print(proto + '://' +src[0] + ':' + src[1])
+
+if __name__ == "__main__":
+    get_list("")
+    for i in range(0,16):
+        get_list(str(i))
